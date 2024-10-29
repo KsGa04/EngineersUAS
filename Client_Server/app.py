c6917bb1fc1472a70e56969601a718aebf77b90f
Smulemun <pokemon88005553535@gmail.com>
2024-10-29 13:58:22 +0300

    добавлены страницы

--- Client_Server/app.py
+++ Client_Server/app.py
@@ -49,8 +49,19 @@ app = create_app(Config)
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/userboard')
+def userboard():
+    return render_template('userboard.html')
 
 if __name__ == "__main__":
     CORS(app)
